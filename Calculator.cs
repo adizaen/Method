0acf0d556a9aab983661156278262f526d2ac6fd
Adi <adi.mustaqim@students.amikom.ac.id>
2019-04-02 14:31:48 +0700

    Latihan Method

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Calculator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        // overload method : menggunakan nama method sama.
+        // tetapi diberi pembeda seperti tipe data/jumlah parameter formal.
+
+        public int Penjumlahan(int a, int b, int c)     
+        {
+            return a + b + c;
+        }
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        // static : hanya bisa diakses oleh class yang mengandung method static itu
+        // dalam hal ini class Calculator
+        public static int Perkalian(int a, int b)   
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Method";
+            // deklarasi variabel
+            int hasil;
+
+            // membuat objek dari class calculator
+            Calculator calculator = new Calculator();
+
+            // panggil method Penjumlahan
+            hasil = calculator.Penjumlahan(10, 2);
+            CetakHasil("Hasil jumlah : " + hasil);
+
+            // panggila method jumlah dengan 3 parameter (overload method)
+            hasil = calculator.Penjumlahan(10, 2, 3);
+            CetakHasil("Hasil penjumlah overload : " + hasil);
+
+            // panggil method pengurangan
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil pengurangan : " + hasil);
+
+            // panggil staticmethod, langung dari nama class nya
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil perkalian : " + hasil);
+
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(string hasil) 
+        {
+            Console.WriteLine(hasil);    
+        }
+    }
+}
